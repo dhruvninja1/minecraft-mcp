6a94f3d79f610b7d0b441bc0fad05279e71efaf8
dhruvninja1 <dhruvninja@icloud.com>
2025-05-28 11:54:24 -0700

    improved margin function

--- hypixel-mcp.py
+++ hypixel-mcp.py
@@ -28,36 +28,58 @@ bzdata = skyblock.get_bazaar_data()["products"]
 
 
 
-def find_margins(product_name="all"):
+def find_margins(product_name : str ="all", top: int = -1, instahourly: float = 0, purse_requirement = "no") -> list or int: #type: ignore
+    if purse_requirement != "no":
+        purse = get_purse("dhruvninja")
+    else:
+        purse = 500000000000000000
     if product_name == "all":
         margins = []
         for key in bzdata:
-            margins.append((key, float(bzdata[key]["quick_status"]["sellPrice"]) - float(bzdata[key]["quick_status"]["buyPrice"])))
-        return margins
+            if (float(bzdata[key]["quick_status"]["buyMovingWeek"]) >= instahourly * 24 * 7 and float(bzdata[key]["quick_status"]["sellMovingWeek"]) >= instahourly * 24 * 7 ) and purse >= float(bzdata[key]["quick_status"]["sellPrice"]):
+                margins.append((key, int(float(bzdata[key]["quick_status"]["buyPrice"]) - float(bzdata[key]["quick_status"]["sellPrice"])))) 
+        if top == -1:
+            return margins    
+        else:
+            n = len(margins)
+            swapped=False
+            for i in range(n-1):
+                swapped = False
+                for j in range(n-1-i):
+                    if margins[j][1] > margins[j+1][1]:
+                        margins[j], margins[j+1] = margins[j+1], margins[j]
+                        swapped=True
+                if not swapped:
+                    break
+            margins.reverse()
+            return margins[0:top]
+
+
     else:
-        return(float(bzdata[product_name]["quick_status"]["sellPrice"]) - float(bzdata[product_name]["quick_status"]["buyPrice"]))
-    
+        return int(float(bzdata[product_name]["quick_status"]["buyPrice"]) - float(bzdata[product_name]["quick_status"]["sellPrice"]))
 
-
-
-
-def get_purse(username, profile="all"):
+def get_purse(username : str, profile : str="selected") -> dict or int: #type: ignore
     playerdata = skyblock.get_player_profile(username)
     uuid = skyblock.get_uuid(username)
     profile = profile.capitalize()
-    coins = {}
+    
+    if profile != "Selected":
+        coins = {}
+        for x in range(len(playerdata["profiles"])):
+            try:
+                coins[playerdata["profiles"][x]["cute_name"]] = playerdata["profiles"][x]["members"][uuid]["currencies"]["coin_purse"]
+            except KeyError:
+                coins[playerdata["profiles"][x]["cute_name"]] = None
+        return int(coins[profile])
 
-    for x in range(len(playerdata["profiles"])):
-        try:
-            coins[playerdata["profiles"][x]["cute_name"]] = playerdata["profiles"][x]["members"][uuid]["currencies"]["coin_purse"]
-        except KeyError:
-            coins[playerdata["profiles"][x]["cute_name"]] = None
-    if profile != "All":
-        return coins[profile]
     else:
-        return coins
+        for x in range(len(playerdata["profiles"])):
+            if playerdata["profiles"][x]["selected"]:
+                selected = x
+                break
+        return int(playerdata["profiles"][selected]["members"][uuid]["currencies"]["coin_purse"])
 
-def data_decode(data):
+def data_decode(data : str) -> str: 
     decoded_data = base64.b64decode(data)
 
     try:
@@ -69,3 +91,4 @@ def data_decode(data):
 
 
 
+print(find_margins(top=10, instahourly=0.7, purse_requirement="dhruvninja"))
